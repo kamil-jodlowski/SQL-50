d28c51db67e70113728308c2162abdeed24a5f22
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-22 18:34:50 +0200

    Create query.sql

---
+++ Customer Who Visited but Did Not Make Any Transactions/query.sql
@@ -0,0 +1,7 @@
+--Customer Who Visited but Did Not Make Any Transactions
+
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions ON (Transactions.visit_id = Visits.visit_id)
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id ;
