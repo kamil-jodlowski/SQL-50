f2a3f2735f127aa56a90de1646d32559340ae2f6
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-22 23:57:55 +0200

    Update query.sql

--- Average Time of Process per Machine/query.sql
+++ Average Time of Process per Machine/query.sql
@@ -1,1 +1,17 @@
+--Average Time of Process per Machine
+
+WITH ProcessTimes AS (
+    SELECT machine_id, process_id , MAX(CASE WHEN activity_type = 'end' THEN timestamp END) - MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS totaltime
+    FROM Activity
+    GROUP BY machine_id, process_id
+),
+
+AverageTimes AS (
+    SELECT machine_id, AVG(totaltime) AS avg_processing_time
+    FROM ProcessTimes
+    GROUP BY machine_id
+)
+SELECT machine_id, ROUND(avg_processing_time, 3) AS processing_time
+FROM AverageTimes
+GROUP BY machine_id;
 
