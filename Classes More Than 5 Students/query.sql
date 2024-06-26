af4e5bd3aa0e127d1cca7c4a1941d2c0e6d5253c
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-06-26 09:52:55 +0200

    Create query.sql

---
+++ Classes More Than 5 Students/query.sql
@@ -0,0 +1,11 @@
+--Classes More Than 5 Students
+
+WITH Tab1 AS (SELECT
+ class,
+ COUNT(student) AS stud_count
+ FROM Courses
+ GROUP BY class)
+
+ SELECT Tab1.class
+ FROM Tab1
+ WHERE Tab1.stud_count >= 5;
